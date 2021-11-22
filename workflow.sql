e6fa2b00ee9df5e56f82a4e8839f455aca23e53d
mbozic987 <mbozic987@gmail.com>
2021-11-22 21:18:42 +0100

    Database modification
    Streamline database layout

--- workflow.sql
+++ workflow.sql
@@ -3,58 +3,45 @@ create database workflow character set utf8;
 use workflow;
 
 create table client(
-    id              int not null primary key auto_increment,
-    firstname       varchar(50) not null,
-    lastname        varchar(50) not null,
-    company         varchar(50),
-    phonenum        varchar(20) not null,
-    email           varchar(50)
+    id                      int not null primary key auto_increment,
+    firstname               varchar(50) not null,
+    lastname                varchar(50) not null,
+    company                 varchar(50),
+    phonenum                varchar(20) not null,
+    email                   varchar(50)
 );
 
 create table employee(
-    id              int not null primary key auto_increment,
-    firstname       varchar(50) not null,
-    lastname        varchar(50) not null,
-    phonenum        varchar(20) not null,
-    email           varchar(50) not null
-);
-
-create table frontdesk(
-    id              int not null primary key auto_increment,
-    employee        int not null
-);
-
-create table repairman(
-    id              int not null primary key auto_increment,
-    employee        int not null,
-    workstation     int not null
-);
-
-create table device(
-    id              int not null primary key auto_increment,
-    client          int not null,
-    frontdesk       int not null,
-    manufacturer    varchar(30) not null,
-    model           varchar(30) not null,
-    serialnum       int,
-    malfunction     varchar(200) not null,
-    receivedate     datetime,
-    repairstatus    boolean
+    id                      int not null primary key auto_increment,
+    firstname               varchar(50) not null,
+    lastname                varchar(50) not null,
+    phonenum                varchar(20) not null,
+    email                   varchar(50) not null
 );
 
 create table workorder(
-    id              int not null primary key auto_increment,
-    device          int not null,
-    repairman       int not null,
-    repairdate      datetime
+    id                      int not null primary key auto_increment,
+    employee_frontdesk      int not null,
+    employee_repairman      int,
+    workstation             int,
+    device                  int not null,
+    malfunction             varchar(200) not null,
+    receive_date            datetime,
+    repair_status           boolean,
+    work_done               varchar(200) not null,
+    repair_date             datetime
 );
 
-alter table frontdesk add foreign key (employee) references employee(id);
+create table device(
+    id                      int not null primary key auto_increment,
+    client                  int not null,
+    manufacturer            varchar(30) not null,
+    model                   varchar(30) not null,
+    serialnum               varchar(20)
+);
 
-alter table repairman add foreign key (employee) references employee(id);
+alter table workorder add foreign key (employee_frontdesk) references employee(id);
+alter table workorder add foreign key (employee_repairman) references employee(id);
+alter table workorder add foreign key (device) references device(id);
 
 alter table device add foreign key (client) references client(id);
-alter table device add foreign key (frontdesk) references frontdesk(id);
-
-alter table workorder add foreign key (device) references device(id);
-alter table workorder add foreign key (repairman) references repairman(id);
