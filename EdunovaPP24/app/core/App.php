c3e4ea9ef393348eb4fb8aa0ad8996dca4dcb466
mbozic987 <mbozic987@gmail.com>
2022-02-14 11:59:31 +0100

    Mechanism for calling method on controller

--- EdunovaPP24/app/core/App.php
+++ EdunovaPP24/app/core/App.php
@@ -5,6 +5,31 @@ class App
     public static function start()
     {
         $route = Request::getRoute();
-        echo $route;
-    }
+        //echo $route;
+
+        $part = explode('/',$route);
+
+        $class = '';
+        if(!isset($part[1]) || $part[1]===''){
+            $class = 'Index';
+        }else{
+            $class = ucfirst($part[1]);
+        }
+
+        $class .= 'Controller';
+
+        $method = '';
+        if(!isset($part[2]) || $part[2]===''){
+            $method = 'Index';
+        }else{
+            $method = $part[2];
+        }
+
+        if(class_exists($class) && method_exists($class,$method)){
+            $instance = new $class();
+            $instance->$method();
+        }else{
+            echo $class . '->' . $method . '() does not exist!';
+        }
+    }   
 }
