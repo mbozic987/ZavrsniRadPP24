7eaa5f7b51d516b3946fec50e4586cb89fbeaecf
mbozic987 <mbozic987@gmail.com>
2022-03-31 21:00:06 +0200

    Workorder read model

--- WorkflowAPP/app/model/Workorder.php
+++ WorkflowAPP/app/model/Workorder.php
@@ -7,15 +7,21 @@ class Workorder
         $conn = DB::getInstance();
         $exp = $conn->prepare('
         
-            select count(a.workorder_id)
+            select a.workorder_id, b.device_id, b.manufacturer, b.model, b.serialnum,
+            c.client_id, c.firstname, c.lastname, c.company, c.phonenum, c.email,
+            d.employee_id as repairman_id, d.firstname as repairman_firstname,
+            e.employee_id as frontdesk_id, e.firstname as frontdesk_firstname,
+            a.malfunction, a.receive_date, f.status_name, a.work_done,
+            a.query_id, a.repair_date
             from workorder a
             inner join device b on a.device = b.device_id
             inner join client c on b.client = c.client_id
-            left join employee d on a.employee_repairman = d.employee_id
-            inner join repair_status e on a.repair_status = e.repair_status_id
+            inner join employee d on a.employee_repairman = d.employee_id
+            inner join employee e on a.employee_frontdesk = e.employee_id
+            inner join repair_status f on a.repair_status = f.repair_status_id
             where concat(b.manufacturer, \' \', b.model, \' \', c.firstname, \' \',
             c.lastname, \' \', ifnull(c.company, \' \'), d.firstname, \' \',
-            d.lastname, \' \', e.status_name) like :cond;
+            d.lastname, \' \', f.status_name) like :cond;
 
         ');
         $cond = '%' . $cond . '%';
@@ -33,8 +39,37 @@ class Workorder
         $conn = DB::getInstance();
         $exp = $conn->prepare('
         
-            select a.workorder_id, d.firstname, 
+            select a.workorder_id, b.device_id, b.manufacturer, b.model, b.serialnum,
+            c.client_id, c.firstname, c.lastname, c.company, c.phonenum, c.email,
+            d.employee_id as repairman_id, d.firstname as repairman_firstname,
+            e.employee_id as frontdesk_id, e.firstname as frontdesk_firstname,
+            a.malfunction, a.receive_date, f.status_name, a.work_done,
+            a.query_id, a.repair_date
+            from workorder a
+            inner join device b on a.device = b.device_id
+            inner join client c on b.client = c.client_id
+            inner join employee d on a.employee_repairman = d.employee_id
+            inner join employee e on a.employee_frontdesk = e.employee_id
+            inner join repair_status f on a.repair_status = f.repair_status_id
+            where concat(b.manufacturer, \' \', b.model, \' \', c.firstname, \' \',
+            c.lastname, \' \', ifnull(c.company, \' \'), d.firstname, \' \',
+            d.lastname, \' \', f.status_name) like :cond
+            group by
+            a.workorder_id, b.device_id, b.manufacturer, b.model, b.serialnum,
+            c.client_id, c.firstname, c.lastname, c.company, c.phonenum, c.email,
+            d.employee_id, d.firstname, e.employee_id, e.firstname,
+            a.malfunction, a.receive_date, f.status_name, a.work_done,
+            a.query_id, a.repair_date
+            order by 21, 19
+            limit :from, :rpp;
+
         
         ');
+        $cond = '%' . $cond . '%';
+        $exp->bindValue('from',$from,PDO::PARAM_INT);
+        $exp->bindValue('rpp',$rpp,PDO::PARAM_INT);
+        $exp->bindParam('cond',$cond);
+        $exp->execute();
+        return $exp->fetchAll();
     }
 }
