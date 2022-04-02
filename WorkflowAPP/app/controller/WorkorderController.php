b661a7b56bfafaec29cc398edb702202264d9dac
mbozic987 <mbozic987@gmail.com>
2022-04-02 19:53:53 +0200

    Workorder read and index

--- WorkflowAPP/app/controller/WorkorderController.php
+++ WorkflowAPP/app/controller/WorkorderController.php
@@ -5,8 +5,64 @@ class WorkorderController extends AuthorizationController
     private$viewDir = 'private' . DIRECTORY_SEPARATOR . 
                         'workorder' . DIRECTORY_SEPARATOR;
 
+    private $message;
+    private $workorder;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->workorder = new stdClass();
+        $this->workorder->workorder_id=0;
+        $this->workorder->device_id=0;
+        $this->workorder->manufacturer='';
+        $this->workorder->model='';
+        $this->workorder->serialnum='';
+        $this->workorder->client_id=0;
+        $this->workorder->firstname='';
+        $this->workorder->lastname='';
+        $this->workorder->company='';
+        $this->workorder->phonemun='';
+        $this->workorder->email='';
+        $this->workorder->repairman_id=0;
+        $this->workorder->repairman_firstname='';
+        $this->workorder->frontdesk_id=0;
+        $this->workorder->frontdesk_firstname='';
+        $this->workorder->malfunction='';
+        $this->workorder->recive_date='';
+        $this->workorder->status_name='';
+        $this->workorder->work_done='';
+        $this->workorder->query_id='';
+        $this->workorder->repair_date='';
+    }
     public function index()
     {
-        $this->view->render($this->viewDir . 'index');
+        if(!isset($_GET['page'])){
+            $page = 1;
+        }else{
+            $page = (int)$_GET['page'];
+        }
+        if($page==0){
+            $page = 1;
+        }
+
+        if(!isset($_GET['cond'])){
+            $cond = '';
+        }else{
+            $cond = $_GET['cond'];
+        }
+
+        $workorederTotal = Workorder::workorderTotal($cond);
+        $pageTotal = ceil($workorderTotal / App::config('rpp'));
+
+        if($page>$pageTotal){
+            $page = $pageTotal;
+        }
+
+        $this->view->render($this->viewDir . 'index',[
+            'entity' => Workorder::read($page,$cond),
+            'cond' => $cond,
+            'page' => $page,
+            'pageTotal' => $pageTotal
+        ]);
     }
 }
